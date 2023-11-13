ca911bec2e88274a12e15bb0f4217dc692297e84
Jerry <jca476@sfu.ca>
2023-11-13 13:04:09 -0800

    Revert "added MUI to project"
    This reverts commit 81375daba40f8ba4623a49cdf25e3a83d0b63935.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import Box from MUI
-
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,9 +5,23 @@ import Foo from './frontend/Foo.js';
 
 function App() {
   return (
-    <Box>
-      <Foo></Foo>
-    </Box>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Foo></Foo>
+      </header>
+    </div>
   );
 }
 
