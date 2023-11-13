81375daba40f8ba4623a49cdf25e3a83d0b63935
Jerry <jca476@sfu.ca>
2023-11-13 12:40:27 -0800

    added MUI to project

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Box from MUI
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,23 +7,9 @@ import Foo from './frontend/Foo.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Foo></Foo>
-      </header>
-    </div>
+    <Box>
+      <Foo></Foo>
+    </Box>
   );
 }
 
