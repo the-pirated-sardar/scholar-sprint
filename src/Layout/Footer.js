dc1fe32da0766cea26eeb3fcdccac00214a935cf
Jerry <jca476@sfu.ca>
2023-11-25 17:27:25 -0800

    boilerplate

---
+++ src/Layout/Footer.js
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material"
+import theme from "../themes/theme"
+
+
+export default function Footer() {
+    return (
+        <Box sx={theme.Footer}>
+            Contact us at
+        </Box>
+    )
+}
--- src/themes/theme.js
+++ src/themes/theme.js
@@ -2,7 +2,6 @@ const theme = {
   rootTheme: {
     width: '100vw',      // 100% of the viewport width
     height: '100vh',     // 100% of the viewport height
-    //backgroundColor: '#41404F', // Background color (adjust as needed)
     background: 'url("appBackground.png") center/cover no-repeat fixed',
     display: 'flex',
     justifyContent: 'center',
